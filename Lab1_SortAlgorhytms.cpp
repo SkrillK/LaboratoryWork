097099c1c5f2f8e964cb869dfae74d61e3dee643
unknown <leonid15072004@gmail.com>
2023-10-29 22:44:52 +0300

    Lab1&2

---
+++ Lab1_SortAlgorhytms.cpp
@@ -0,0 +1,347 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 200;
+
+void comps_and_swaps_out(int comps, int swaps)
+{
+    cout << "Сравнения: " << comps << endl;
+    cout << "Перестановки: " << swaps << "\n\n";
+}
+
+// ---------------------------------------------------
+
+void SelectionSort(int a[])
+{
+    int ind, arr[N], comps = 0, swaps = 0;
+
+    for (int i = 0; i < N; i++)
+        arr[i] = a[i];
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        ind = i;
+        for (int j = i + 1; j < N; j++)
+        {
+            if (arr[j] < arr[ind])
+                ind = j;
+            comps++;
+        }
+        swap(arr[i], arr[ind]);
+        swaps++;
+    }
+
+    comps_and_swaps_out(comps, swaps);
+}
+
+// ---------------------------------------------------
+
+void BubbleSort(int a[])
+{
+    int arr[N], comps = 0, swaps = 0;
+
+    for (int i = 0; i < N; i++)
+        arr[i] = a[i];
+
+    for (int i = 0; i < N - 1; i++)
+        for (int j = N - 1; j > i; j--) 
+        {
+            if (arr[j - 1] > arr[j])
+            {
+                swap(arr[j - 1], arr[j]);
+                swaps++;
+            }
+            comps++;
+        }
+
+    comps_and_swaps_out(comps, swaps);
+}
+
+// ---------------------------------------------------
+
+void InsertionSort(int a[])
+{
+    int buff, j, arr[N], comps = 0, swaps = 0;
+
+    for (int i = 0; i < N; i++)
+        arr[i] = a[i];
+
+    for (int i = 1; i < N; i++)
+    {
+        j = i;
+        buff = arr[i];
+        while (j > 0 && buff < arr[j - 1])
+        {
+            arr[j] = arr[j - 1];
+            j--;
+            swaps++;
+            comps++;
+        }
+        arr[j] = buff;
+        swaps++;
+    }
+
+    comps_and_swaps_out(comps, swaps);
+}
+
+// ---------------------------------------------------
+
+void BinaryInputSort(int a[])
+{
+    int left, right, m, x, arr[N], comps = 0, swaps = 0;
+
+    for (int i = 0; i < N; i++)
+        arr[i] = a[i];
+
+    for (int i = 1; i < N; i++)
+    {
+        x = arr[i];
+        left = 0;
+        right = i - 1;
+        while (left <= right)
+        {
+            m = (left + right) / 2;
+            (x < arr[m]) ? right = m - 1 : left = m + 1;
+            comps++;
+        }
+        for (int j = i - 1; j >= left; j--)
+        {
+            arr[j + 1] = arr[j];
+            swaps++;
+        }
+        arr[left] = x;
+        swaps++;
+    }
+
+    comps_and_swaps_out(comps, swaps);
+}
+
+// ---------------------------------------------------
+
+void ShakerSort(int a[])
+{
+    int left = 1, right = N - 1, k = N - 1, arr[N], comps = 0, swaps = 0;
+
+    for (int i = 0; i < N; i++)
+        arr[i] = a[i];
+
+    while (left <= right)
+    {
+        for (int j = right; j >= left; j--)
+        {
+            if (arr[j - 1] > arr[j])
+            {
+                swap(arr[j - 1], arr[j]);
+                k = j;
+                swaps++;
+            }
+            comps++;
+        }
+        left = k + 1;
+        for (int j = 1; j <= right; j++) 
+        {
+            if (arr[j - 1] > arr[j])
+            {
+                swap(arr[j - 1], arr[j]);
+                k = j;
+                swaps++;
+            }
+            comps++;
+        }
+        right = k - 1;
+        comps++;
+    }
+
+    comps_and_swaps_out(comps, swaps);
+}
+
+// ---------------------------------------------------
+
+void ShellSort(int a[])
+{
+    int arr[N], comps = 0, swaps = 0;
+
+    for (int i = 0; i < N; i++)
+        arr[i] = a[i];
+
+    for (int gap = N / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < N; ++i) {
+            int buff = arr[i], j;
+
+            for (j = i; j >= gap && arr[j - gap] > buff; j -= gap)
+            {
+                arr[j] = arr[j - gap];
+                swaps++;
+            }
+            comps++;
+            arr[j] = buff;
+            swaps++;
+        }
+    }
+
+    comps_and_swaps_out(comps, swaps);
+}
+
+// ---------------------------------------------------
+
+int Partition(int arr[], int low, int high, int& comps, int& swaps)
+{
+    int pivot = arr[high];
+    int i = low;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            swap(arr[j], arr[i]);
+            i++;
+            swaps++;
+        }
+        comps++;
+    }
+
+    swap(arr[i], arr[high]);
+    swaps++;
+    return i;
+}
+
+void QS(int arr[], int low, int high, int& comps, int& swaps)
+{
+    if (low >= high)
+        return;
+
+    QS(arr, low, Partition(arr, low, high, comps, swaps) - 1, comps, swaps);
+    QS(arr, Partition(arr, low, high, comps, swaps) + 1, high, comps, swaps);
+}
+
+void QuickSort(int a[]) 
+{
+    int arr[N], comps = 0, swaps = 0;
+
+    for (int i = 0; i < N; i++)
+        arr[i] = a[i];
+    QS(arr, 0, N - 1, comps, swaps);
+
+    comps_and_swaps_out(comps, swaps);
+}
+
+// ---------------------------------------------------
+
+void Heapify(int arr[], int n, int i, int& comps, int& swaps) {
+    int largest = i, left = 2 * i + 1, right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    comps++;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+    comps++;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        swaps++;
+        Heapify(arr, n, largest, comps, swaps);
+    }
+}
+
+void HeapSort(int a[]) 
+{
+    int arr[N], comps = 0, swaps = 0;
+
+    for (int i = 0; i < N; i++)
+        arr[i] = a[i];
+
+    for (int i = N / 2 - 1; i >= 0; --i)
+        Heapify(arr, N, i, comps, swaps);
+
+    for (int i = N - 1; i >= 0; --i) 
+    {
+        swap(arr[0], arr[i]);
+        swaps++;
+        Heapify(arr, i, 0, comps, swaps);
+    }
+
+    comps_and_swaps_out(comps, swaps);
+}
+
+// ---------------------------------------------------
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int rand_arr[N], down_arr[N], up_arr[N];
+
+    for (int i = 0; i < N; i++)
+        rand_arr[i] = rand() % 200;
+
+    for (int i = N - 1; i >= 0; i--)
+        down_arr[i] = N - i;
+
+    for (int i = 0; i < N; i++)
+        up_arr[i] = i + 1;
+
+    cout << "Сортировка простым выбором" << endl;
+    cout << "Случайный массив:" << endl;
+    SelectionSort(rand_arr);
+    cout << "Убывающий массив:" << endl;
+    SelectionSort(down_arr);
+    cout << "Возрастающий массив:" << endl;
+    SelectionSort(up_arr);
+
+    cout << "------------------------------\nСортировка простым обменом" << endl;
+    cout << "Случайный массив:" << endl;
+    BubbleSort(rand_arr);
+    cout << "Убывающий массив:" << endl;
+    BubbleSort(down_arr);
+    cout << "Возрастающий массив:" << endl;
+    BubbleSort(up_arr);
+
+    cout << "------------------------------\nСортировка простой вставки" << endl;
+    cout << "Случайный массив:" << endl;
+    InsertionSort(rand_arr);
+    cout << "Убывающий массив:" << endl;
+    InsertionSort(down_arr);
+    cout << "Возрастающий массив:" << endl;
+    InsertionSort(up_arr);
+
+    cout << "------------------------------\nСортировка бинарной вставки" << endl;
+    cout << "Случайный массив:" << endl;
+    BinaryInputSort(rand_arr);
+    cout << "Убывающий массив:" << endl;
+    BinaryInputSort(down_arr);
+    cout << "Возрастающий массив:" << endl;
+    BinaryInputSort(up_arr);
+
+    cout << "------------------------------\nШейкер-сортировка" << endl;
+    cout << "Случайный массив:" << endl;
+    ShakerSort(rand_arr);
+    cout << "Убывающий массив:" << endl;
+    ShakerSort(down_arr);
+    cout << "Возрастающий массив:" << endl;
+    ShakerSort(up_arr);
+
+    cout << "------------------------------\nСортировка Шелла" << endl;
+    cout << "Случайный массив:" << endl;
+    ShellSort(rand_arr);
+    cout << "Убывающий массив:" << endl;
+    ShellSort(down_arr);
+    cout << "Возрастающий массив:" << endl;
+    ShellSort(up_arr);
+
+    cout << "------------------------------\nБыстрая сортировка" << endl;
+    cout << "Случайный массив:" << endl;
+    QuickSort(rand_arr);
+    cout << "Убывающий массив:" << endl;
+    QuickSort(down_arr);
+    cout << "Возрастающий массив:" << endl;
+    QuickSort(up_arr);
+
+    cout << "------------------------------\nСортировка куч" << endl;
+    cout << "Случайный массив:" << endl;
+    HeapSort(rand_arr);
+    cout << "Убывающий массив:" << endl;
+    HeapSort(down_arr);
+    cout << "Возрастающий массив:" << endl;
+    HeapSort(up_arr);
+}
---
+++ Lab2_RecursiveAlgorhytms.cpp
@@ -0,0 +1,223 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void chessBoardOut(int, int**);
+
+bool nextMoveCheck(int x, int y, int n)
+{
+    return (x >= 0 && x < n && y >= 0 && y < n);
+}
+
+void knightsNextMove(int** chessBoard, int x, int y, int numOfMove, int n)
+{
+    chessBoard[x][y] = numOfMove;
+    int moveX[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
+    int moveY[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+
+    for (int i = 0; i < 8; i++)
+    {
+        int nextMoveX = x + moveX[i];
+        int nextMoveY = y + moveY[i];
+
+        if (nextMoveCheck(nextMoveX, nextMoveY, n) && chessBoard[nextMoveX][nextMoveY] == 0)
+            knightsNextMove(chessBoard, nextMoveX, nextMoveY, numOfMove + 1, n);
+    }
+}
+
+int** knightsMove(int n, int startPosX, int startPosY)
+{
+    int** chessBoard = new int* [n];
+
+    for (int i = 0; i < n; i++)
+    {
+        chessBoard[i] = new int[n];
+        for (int j = 0; j < n; j++)
+            chessBoard[i][j] = 0;
+    }
+    knightsNextMove(chessBoard, startPosX, startPosY, 1, n);
+
+    return chessBoard;
+}
+
+void knightsMoveStart()
+{
+    int n, startPosX, startPosY;
+
+    cout << "\nSize of chess board: ";
+    cin >> n;
+
+    cout << "\nKnight's starting position:\nX = ";
+    cin >> startPosX;
+    cout << "Y = ";
+    cin >> startPosY;
+
+    int** chessBoard = knightsMove(n, startPosX - 1, startPosY - 1);
+
+    chessBoardOut(n, chessBoard);
+}
+
+//---------------------------------------------------------------------
+
+bool placingSafetyCheck(int row, int col, int **chessBoard, int N)
+{
+    for (int i = 0; i < col; i++)
+        if (chessBoard[row][i])
+            return false;
+
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (chessBoard[i][j])
+            return false;
+
+    for (int i = row, j = col; i < N && j >= 0; i++, j--)
+        if (chessBoard[i][j])
+            return false;
+
+    return true;
+}
+
+bool queenPlacing(int col, int** chessBoard, int n)
+{
+    if (col == n)
+        return true;
+    for (int i = 0; i < n; i++)
+    {
+        if (placingSafetyCheck(i, col, chessBoard, n))
+        {
+            chessBoard[i][col] = 1;
+            if (queenPlacing(col + 1, chessBoard, n))
+                return true;
+            chessBoard[i][col] = 0;
+        }
+    }
+
+    return false;
+}
+
+void queenPlacingStart()
+{
+    int n;
+
+    cout << "\nSize of chess board: ";
+    cin >> n;
+
+    int** chessBoard = new int* [n];
+    for (int i = 0; i < n; i++)
+        chessBoard[i] = new int[n];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            chessBoard[i][j] = 0;
+
+    if (queenPlacing(0, chessBoard, n))
+        chessBoardOut(n, chessBoard);
+    else
+        cout << "No solution.";
+}
+
+//---------------------------------------------------------------------
+
+void moveDisk(int n, int from, int buff, int to, int& movesCount) {
+    if (n == 1) {
+        movesCount++;
+        cout << "Disk:  " << n << "\tPeg: " << from << " -> " << to << endl;
+        return;
+    }
+    moveDisk(n - 1, from, to, buff, movesCount);
+    cout << "Disk:  " << n << "\tPeg: " << from << " -> " << to << endl;
+    movesCount++;
+    moveDisk(n - 1, buff, from, to, movesCount);
+}
+
+void hanoyTowersStart() {
+    int n, movesCount = 0;
+
+    cout << "\nAmount of disks: ";
+    cin >> n;
+
+    moveDisk(n, 1, 2, 3, movesCount);
+    cout << "Count of moves: " << movesCount << "\n\n";
+}
+
+//---------------------------------------------------------------------
+
+int maxEl(int* arr, int left, int right)
+{
+    int mid, leftMax, rightMax;
+    if (left == right) 
+        return arr[left];
+
+    mid = (left + right) / 2;
+    leftMax = maxEl(arr, left, mid);
+    rightMax = maxEl(arr, mid + 1, right);
+
+    return max(leftMax, rightMax);
+}
+
+void findMaxArrEl()
+{
+    int n;
+
+    cout << "\nAmount of elements: ";
+    cin >> n;
+
+    int* array = new int[n];
+    cout << "\nRandom array:\n";
+    for (int i = 0; i < n; i++) {
+        array[i] = rand() % 100 - 50;
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+
+    cout << "Max element: " << maxEl(array, 0, n - 1) << "\n\n";
+}
+
+//---------------------------------------------------------------------
+
+void chessBoardOut(int n, int **chessBoard)
+{
+    cout << "\nChess board:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << chessBoard[i][j] << "\t";
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void algorhytmChoise()
+{
+    int n = 0, answer = 0;
+
+    cout << "//----------------------------------------------------------\n\n";
+
+    cout << "Choose the algorhytm.\n 1. Queen's placing\n 2. Knight's move\n";
+    cout << " 3. Hanoy towers\n 4. Max element\nYour choise: ";
+    cin >> answer;
+
+    switch (answer) {
+    default:
+        return;
+        break;
+    case 1:
+        queenPlacingStart();
+        break;
+    case 2:
+        knightsMoveStart();
+        break;
+    case 3:
+        hanoyTowersStart();
+        break;
+    case 4:
+        findMaxArrEl();
+        break;
+    }
+
+    algorhytmChoise();
+}
+
+int main()
+{
+    algorhytmChoise();
+}
